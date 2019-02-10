341f71cbd9ba889fe6d7532993dd2c1dc4b03709
Amitav Roy <reachme@amitavroy.com>
2019-02-10 17:33:12 +0530

    added code for starting and stopping ec2 instances using Lamda functions

---
+++ aws/README.md
@@ -0,0 +1,3 @@
+This folder contains some scripts which we can run to manage EC2 instances.
+
+1. Start EC2 instances - this can be used to create a Lamda function along with CloudWatch to start an instance at a particular time.
---
+++ aws/stop_ec2_instances.py
@@ -0,0 +1,12 @@
+import boto3
+# Enter the region your instances are in. 
+# Include only the region without specifying Availability Zone; e.g., 'us-east-1'
+region = 'ap-southeast-1'
+
+# Enter your instances here: ex. ['X-XXXXXXXX', 'X-XXXXXXXX']
+instances = ['i-XXXXXXXX', 'i-XXXXXXXX']
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
+    print 'stopped your instances: ' + str(instances)
